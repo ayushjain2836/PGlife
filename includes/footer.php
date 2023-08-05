a69834a7433fd08c5a635f605b3411545b5a1340
ayush jain <63713469+ayushjain2836@users.noreply.github.com>
2023-08-05 15:46:20 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="property_list.php?city=indore">PG in indore</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=gwalior">PG in gwalior</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=bhopal">PG in bhopal</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=rewa">PG in rewa</a>
+            </div>
+        </div>
+        <div class="footer-copyright"> PG Life live fullist</div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
